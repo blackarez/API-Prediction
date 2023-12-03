0c5806acb1edef91e6151a2698dc18f2e06b4725
blackarez <angellg1990@gmail.com>
2023-12-03 13:57:33 -0500

    Update main.py
    transformar tipos de cancer de numero a texto

--- main.py
+++ main.py
@@ -33,6 +33,17 @@ class ItemRL(BaseModel):
 
 listaTipeCancerUnic = np.array([ 8,  9, 24, 26, 19, 20, 23, 15, 18, 16, 11,  6, 13, 14,  2,  0,  3,
         5, 10,  7, 12, 22, 21,  4,  1, 25, 17])
+listaTipeCancer = np.array(['Kidney cancer', 'Larynx cancer', 'Thyroid cancer',
+       'Uterine cancer', 'Ovarian cancer', 'Pancreatic cancer',
+       'Testicular cancer', 'Nasopharynx cancer', 'Other pharynx cancer',
+       'Non-melanoma skin cancer', 'Lip and oral cavity cancer',
+       'Gallbladder and biliary tract cancer', 'Malignant skin melanoma',
+       'Multiple myeloma', 'Breast cancer', 'Bladder cancer',
+       'Cervical cancer', 'Esophageal cancer', 'Leukemia',
+       'Hodgkin lymphoma', 'Liver cancer', 'Stomach cancer',
+       'Prostate cancer', 'Colon and rectum cancer',
+       'Brain and nervous system cancer',
+       'Tracheal, bronchus, and lung cancer', 'Other cancers'])
 
 def dataPrediction(country, ano):
     listaCountry = np.array(['Year', 'Tipo de Cancer', 'Code_AFG', 'Code_AGO', 'Code_ALB', 'Code_AND', 'Code_ANZ', 'Code_AR ', 'Code_ARE', 'Code_ARG', 'Code_ARM', 'Code_ASM', 'Code_ATG', 'Code_AUS', 'Code_AUT', 'Code_AZE', 'Code_BDI', 'Code_BEL', 'Code_BEN', 'Code_BFA', 'Code_BGD', 'Code_BGR', 'Code_BHR', 'Code_BHS', 'Code_BIH', 'Code_BLR', 'Code_BLZ', 'Code_BMU', 'Code_BOL', 'Code_BRB', 'Code_BRN', 'Code_BTN', 'Code_BWA', 'Code_CAF', 'Code_CAN', 'Code_CAR ', 'Code_CHE', 'Code_CHL', 'Code_CIV', 'Code_CMR', 'Code_COD', 'Code_COG', 'Code_COL', 'Code_COM', 'Code_CPV', 'Code_CRI', 'Code_CSSA', 'Code_CUB', 'Code_CYP', 'Code_CZE', 'Code_DJI', 'Code_DMA', 'Code_DNK', 'Code_DOM', 'Code_DZA', 'Code_ECU', 'Code_EGY', 'Code_ENG', 'Code_ERI', 'Code_ESP', 'Code_EST', 'Code_ETH', 'Code_FIN', 'Code_FJI', 'Code_FRA', 'Code_FSM', 'Code_GAB', 'Code_GBR', 'Code_GEO', 'Code_GHA', 'Code_GIN', 'Code_GMB', 'Code_GNB', 'Code_GNQ', 'Code_GRC', 'Code_GRD', 'Code_GRL', 'Code_GTM', 'Code_GUM', 'Code_GUY', 'Code_HND', 'Code_HRV', 'Code_HTI', 'Code_HUN', 'Code_IRL', 'Code_IRN', 'Code_IRQ', 'Code_ISL', 'Code_ISR', 'Code_ITA', 'Code_JAM', 'Code_JOR', 'Code_JPN', 'Code_KAZ', 'Code_KEN', 'Code_KGZ', 'Code_KHM', 'Code_KIR', 'Code_KOR', 'Code_KWT', 'Code_LAO', 'Code_LBN', 'Code_LBR', 'Code_LBY', 'Code_LCA', 'Code_LKA', 'Code_LSO', 'Code_LTU', 'Code_LUX', 'Code_LVA', 'Code_MAR', 'Code_MDA', 'Code_MDG', 'Code_MDV', 'Code_MHL', 'Code_MKD', 'Code_MLI', 'Code_MLT', 'Code_MMR', 'Code_MNE', 'Code_MNG', 'Code_MNP', 'Code_MOZ', 'Code_MRT', 'Code_MUS', 'Code_MWI', 'Code_MYS', 'Code_NAM', 'Code_NER', 'Code_NGA', 'Code_NIC', 'Code_NIR', 'Code_NLD', 'Code_NOR', 'Code_NPL', 'Code_NZL', 'Code_OC', 'Code_OMN', 'Code_PAK', 'Code_PAN', 'Code_PER', 'Code_PHL', 'Code_PNG', 'Code_POL', 'Code_PRI', 'Code_PRK', 'Code_PRT', 'Code_PRY', 'Code_PSE', 'Code_QAT', 'Code_ROU', 'Code_RWA', 'Code_SAU', 'Code_SCT', 'Code_SDN', 'Code_SEN', 'Code_SGP', 'Code_SLB', 'Code_SLE', 'Code_SLV', 'Code_SOM', 'Code_SRB', 'Code_SSD', 'Code_SSSA', 'Code_STP', 'Code_SUR', 'Code_SVK', 'Code_SVN', 'Code_SWE', 'Code_SWZ', 'Code_SYC', 'Code_SYR', 'Code_TCD', 'Code_TGO', 'Code_THA', 'Code_TJK', 'Code_TKM', 'Code_TLS', 'Code_TON', 'Code_TTO', 'Code_TUN', 'Code_TWN', 'Code_TZA', 'Code_UGA', 'Code_UKR', 'Code_URY', 'Code_UZB', 'Code_VCT', 'Code_VEN', 'Code_VIR', 'Code_VNM', 'Code_VUT', 'Code_WA', 'Code_WSM', 'Code_YEM', 'Code_ZAF', 'Code_ZMB', 'Code_ZWE'])
@@ -47,6 +58,7 @@ async def root():
     await asyncio.sleep(1)
     model_response = model.predict(dataPrediction('COL', 2050))
     resultados = pd.DataFrame({'Tipo de Cancer': listaTipeCancerUnic, 'Probabilidad de Mortalidad': model_response})
+    resultados['Tipo de Cancer'] = listaTipeCancer[resultados.index ]
     return {"message": resultados.to_json()}
 
 @app.post("/predict")
@@ -55,6 +67,7 @@ async def predict(item: Item):
     await asyncio.sleep(1)
     model_response = model.predict(dataPrediction(item.country, item.ano))
     resultados = pd.DataFrame({'Tipo de Cancer': listaTipeCancerUnic, 'Probabilidad de Mortalidad': model_response})
+    resultados['Tipo de Cancer'] = listaTipeCancer[resultados.index ]
     return {"message": resultados.to_json()}
 
 @app.get("/test")
